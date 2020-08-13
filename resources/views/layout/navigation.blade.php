929e042a700b266a3026be7905caa4ecad610eb9
roudhi_benaputra <roudhi.benaputra@gmail.com>
2020-08-13 23:01:08 +0700

    Navbar

---
+++ resources/views/layout/navigation.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg bg-danger navbar-dark">
+    <span class="navbar-brand mb-0 h1" href="#">Inventory</span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="fon">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Account</a>
+        </li>
+      </ul>
+    </div>
+</nav>
