8f2ef8811473c5573295659a7a69001464cce0d4
Benaputra <34600767+Benaputra@users.noreply.github.com>
2020-08-14 22:32:46 +0700

    hehe

--- resources/views/master/css_master.blade.php
+++ resources/views/master/css_master.blade.php
@@ -1,1 +1,14 @@
-<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+ <!-- Bootstrap -->
+ <link href="gentella/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+ <!-- Font Awesome -->
+ {{-- <link href="gentella/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet"> --}}
+
+ <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+ <!-- NProgress -->
+ <link href="gentella/vendors/nprogress/nprogress.css" rel="stylesheet">
+ <!-- Animate.css -->
+ <link href="gentella/vendors/animate.css/animate.min.css" rel="stylesheet">
+
+ <!-- Custom Theme Style -->
+ <link href="gentella/build/css/custom.min.css" rel="stylesheet">
